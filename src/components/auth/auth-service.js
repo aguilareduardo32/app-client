c56869a0ef48f6abfd4c5a4732886d7f95e0997b
compuYael <aguilar.eduardo32@yahoo.com>
2020-04-22 16:59:32 -0500

    A

--- src/components/auth/auth-service.js
+++ src/components/auth/auth-service.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 class AuthService {
     constructor() {
         let service = axios.create({
-            baseURL:`${process.env.REACT_APP_API_URL}/auth`,
+            baseURL:`${process.env.REACT_APP_API_URL}/`,
             withCredentials: true
         });
         this.service = service

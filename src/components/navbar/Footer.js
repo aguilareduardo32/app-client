142828f031e96398248c789ce0f5fa9edd0a3bcd
compuYael <aguilar.eduardo32@yahoo.com>
2020-04-22 12:51:55 -0500

    A

--- src/components/navbar/Footer.js
+++ src/components/navbar/Footer.js
@@ -26,7 +26,7 @@ class Fooot extends Component {
                     <footer >
                         
                                 <Link   to='/'>
-                                    <button className="loginsub2" onClick={() => this.logoutUser()}>Loaagout</button>
+                                    <button className="loginsub2" onClick={() => this.logoutUser()}>Loaagit gout</button>
                                 </Link>
                           
                     </footer>
